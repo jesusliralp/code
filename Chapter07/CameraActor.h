6be0b03c29021448e6f77a71319fc1001bfaa29d
jfernando.lirap <jfernando.lirap@gmail.com>
2024-12-03 22:40:46 -0600

    CameraActor preparado para ejercicio 7.2

--- Chapter07/CameraActor.h
+++ Chapter07/CameraActor.h
@@ -19,9 +19,11 @@ public:
 	void ActorInput(const uint8_t* keys) override;
 
 	void SetFootstepSurface(float value);
+	const Vector3& GetCameraPosition() const { return mCameraPos; }
 private:
 	class MoveComponent* mMoveComp;
 	class AudioComponent* mAudioComp;
+	Vector3 mCameraPos;
 	SoundEvent mFootstep;
 	float mLastFootstep;
 };
